ba47ec134c9e3aea6e8f1ef6c2c2aca3857a22ff
Adrian Estevez Alvarez <adrianchdev@gmail.com>
2026-01-13 11:55:20 -0500

    fix: merge conflicts fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import AdmissionForm from './features/admission/components/AdmissionForm'
-
-function App() {
-  return <AdmissionForm />
-=======
 import Login from './pages/Login'
 
 function App() {
   return <Login />
->>>>>>> main
 }
 
 export default App
